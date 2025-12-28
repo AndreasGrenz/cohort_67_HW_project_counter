ab3139f04d5a5bc2b52d40e767d9d353a21c2480
NataliaMishukova <natali-7-1@mail.ru>
2025-12-29 00:41:43 +0300

    delete logic

--- index.js
+++ index.js
@@ -4,16 +4,3 @@ const decrementBtn = document.getElementById("decrement");
 
 let counter = 0;
 
-function updateCounter() {
-  resultElement.textContent = counter;
-}
-
-incrementBtn.addEventListener("click", function () {
-  counter++;
-  updateCounter();
-});
-
-decrementBtn.addEventListener("click", function () {
-  counter--;
-  updateCounter();
-});
